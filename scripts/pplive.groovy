0b025745774bde7bed7a9db44e2b6392b22e8156
chocolateboy <chocolate@cpan.org>
2011-02-05 15:18:11 +0000

    clean up scripts

--- scripts/pplive.groovy
+++ scripts/pplive.groovy
@@ -1,11 +1,13 @@
 script {
+    PPLIVE = 'C:\\Program Files (x86)\\PPLive\\PPTV\\PPLive.exe'
+
     profile ('PPLive') {
         pattern {
            protocol 'synacast'
         }
 
         action {
-            $HOOK = "C:\\Program Files (x86)\\PPLive\\PPTV\\PPLive.exe ${$URI}"
+            $HOOK = "$PPLIVE \"${$URI}\""
             $URI = 'http://127.0.0.1:8888'
         }
     }
--- scripts/rtmp.groovy
+++ scripts/rtmp.groovy
@@ -1,15 +1,16 @@
 script {
-	profile ('RTMP(E)') {
-		pattern {
-			protocol ([ 'rtmp', 'rtmpe' ])
-		}
+    RTMPDUMP = 'C:\\rtmpdump.exe'
 
-		action {
-			$URI = "\""+$URI+"\""
-			def $X=$URI.split("!!!pms_ch_dash_y!!!") as String[]
-			if($X.length>1)
-				$URI=$X[0]+"\""+" -y \""+$X[1]
-			$DOWNLOADER = "C:\\rtmpdump.exe -o $DOWNLOADER_OUT -r ${$URI}"
-		}
-	}
+    profile ('RTMP(E)') {
+        pattern {
+            protocol ([ 'rtmp', 'rtmpe' ])
+        }
+
+        action {
+            def strings = $URI.split('!!!pms_ch_dash_y!!!')
+            if (strings.length > 1)
+                $URI = strings[0] + '" -y "' + strings[1]
+            $DOWNLOADER = "$RTMPDUMP -o $DOWNLOADER_OUT -r \"${$URI}\""
+        }
+    }
 }
