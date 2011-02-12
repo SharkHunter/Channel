01b23f4ad3834529bfec161ecc900da89f228a97
SharkHunter <bajen.hasse@gmail.com>
2011-02-12 15:19:47 +0100

    Added NaviX

---
+++ channels/navix.ch
@@ -0,0 +1,14 @@
+version=0.1
+channel NaviX {
+	#img=http://svtplay.se/img/brand/svt-play.png
+	folder {
+		name=Site scraper
+		url=http://navix.turner3d.net/playlist/2229/realtime_scrapers.plx
+		type=navix
+	}
+	folder {
+		name=Sample playlist
+		url=http://navi-x.googlecode.com/svn/trunk/Navi-X/examples/example%202.plx
+		type=navix
+	}
+}
--- scripts/rtmp.groovy
+++ scripts/rtmp.groovy
@@ -10,6 +10,9 @@ script {
             def strings = $URI.split('!!!pms_ch_dash_y!!!')
             if (strings.length > 1)
                 $URI = strings[0] + '" -y "' + strings[1]
+			def strings1 = $URI.split('!!!pms_ch_dash_w!!!')
+			if (strings.length > 1)
+                $URI = strings[0] + '" -W "' + strings[1]
             $DOWNLOADER = "$RTMPDUMP -o $DOWNLOADER_OUT -r \"${$URI}\""
         }
     }
