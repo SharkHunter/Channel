86bcea749bfdef4fd184ce620f1a09b9f869e83f
SharkHunter <bajen.hasse@gmail.com>
2011-04-28 16:50:20 +0200

    Touch

--- scripts/curl.groovy
+++ scripts/curl.groovy
@@ -1,11 +1,11 @@
 script {
-	def CURL = 'c:\\curl\\curl.exe'
-	def cf = 'c:\\tmp\\inst_tst\\cookies'
 	profile ('CURL') {
 		pattern {         
 			domain 'megaupload.com'
 		}
 		action {
+			def CURL = pmsConf['curl.path']
+			def cf = pmsConf['cookie.path']
 			$DOWNLOADER = "$CURL -s -S --cookie $cf --location-trusted --output $DOWNLOADER_OUT ${$URI}"       
 		}
 	}
