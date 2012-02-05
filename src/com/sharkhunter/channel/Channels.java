bdfd941dc59145495019a6aac3ae6035ce7f22d5
SharkHunter <bajen.hasse@gmail.com>
2012-02-05 14:01:08 +0100

    RapidShare premium

--- src/com/sharkhunter/channel/Channels.java
+++ src/com/sharkhunter/channel/Channels.java
@@ -28,7 +28,7 @@ import no.geosoft.cc.io.FileMonitor;
 public class Channels extends VirtualFolder implements FileListener {
 
 	// Version string
-	public static final String VERSION="1.56a";
+	public static final String VERSION="1.56";
 	
 	// Constants for RTMP string constructions
 	public static final int RTMP_MAGIC_TOKEN=1;
