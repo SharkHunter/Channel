3d1a67b4b074e1ce8b7d1f3642d59d754db4fff1
SharkHunter <bajen.hasse@gmail.com>
2012-10-10 19:50:16 +0200

    Update installer settings

--- src/com/sharkhunter/channel/Channels.java
+++ src/com/sharkhunter/channel/Channels.java
@@ -36,8 +36,8 @@ import no.geosoft.cc.io.FileMonitor;
 public class Channels extends VirtualFolder implements FileListener {
 
 	// Version string
-	public static final String VERSION="2.02a";
-	public static final String ZIP_VER="200";
+	public static final String VERSION="2.03";
+	public static final String ZIP_VER="203";
 	
 	// Constants for RTMP string constructions
 	public static final int RTMP_MAGIC_TOKEN=1;
