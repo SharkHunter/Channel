9642d94f907164e90d43b086de5747d2bfb3efda
bartsimp <ilnazgul@yahoo.com>
2015-02-27 15:27:39 +0100

    fix parse media section multiple

--- src/com/sharkhunter/channel/ChannelUtil.java
+++ src/com/sharkhunter/channel/ChannelUtil.java
@@ -273,17 +273,17 @@ public class ChannelUtil {
 	
 	public static ArrayList<String> gatherBlock(String[] lines,int start) {
 		ArrayList<String> res=new ArrayList<String>();
-		int curls=0;
+		int curls=1;
 		for(int i=start;i<lines.length;i++) {
 			String str=lines[i].trim();
 			if(str.startsWith("}")) {
-				res.add("}");
 				curls--;
 				if(curls==0)
 					break;
+				res.add("}");
 				continue;
 			}
-			if(str.contains("{"))
+			if(str.endsWith("{"))
 				curls++;
 			res.add(str+"\n");
 		}
@@ -292,17 +292,17 @@ public class ChannelUtil {
 	
 	public static ArrayList<String> gatherBlock(ArrayList<String> data,int start) {
 		ArrayList<String> res=new ArrayList<String>();
-		int curls=0;
+		int curls=1;
 		for(int i=start;i<data.size();i++) {
 			String str=data.get(i).trim();
 			if(str.startsWith("}")) {
-				res.add("}");
 				curls--;
 				if(curls==0)
 					break;
+				res.add("}");
 				continue;
 			}
-			if(str.contains("{"))
+			if(str.endsWith("{"))
 				curls++;
 			res.add(str+"\n");
 		}
